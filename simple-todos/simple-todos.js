8aae5a2c3a28ec519638acabcee7f1e5c687b854
Kyle Mitofsky <KyleMit@gmail.com>
2015-02-10 17:02:29 -0500

    Tutorial Step 1

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,19 +1,12 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    // this code only runs on the client
+    Template.body.helpers({
+        tasks: [
+            { text: 'Task 1' },
+            { text: 'Task 2' },
+            { text: 'Task 4' }
+        ]
+    });
 }
 
 if (Meteor.isServer) {
