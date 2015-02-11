ed480375d60698d53ae03a66ed6fc7a67cb38e29
Kyle Mitofsky <KyleMit@gmail.com>
2015-02-10 22:51:58 -0500

    Tutorial Step 3

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,16 +1,18 @@
+Items = new Mongo.Collection("items");
+
 if (Meteor.isClient) {
     // this code only runs on the client
     Template.body.helpers({
-        tasks: [
-            { text: 'Task 1' },
-            { text: 'Task 2' },
-            { text: 'Task 4' }
-        ]
+        items: function () {
+            return Items.find({});
+        }
     });
+
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
