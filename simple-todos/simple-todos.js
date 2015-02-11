1fe46ee58b750dd1180fa4d88a40246a3ea0ed28
Kyle Mitofsky <KyleMit@gmail.com>
2015-02-10 23:20:44 -0500

    Tutorial - Step 4

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,11 +4,29 @@ if (Meteor.isClient) {
     // this code only runs on the client
     Template.body.helpers({
         items: function () {
-            return Items.find({});
+            return Items.find({}, {sort: {createdAt: -1}});
         }
     });
 
+    Template.body.events({
+        "submit .new-item": function (event) {
+            // this funtion is called when the new task form is submitted
+            var form = event.target;
+            var inputField = form.text;
+            var text = inputField.value;
+            console.log(event);
+            Items.insert({
+                text: text,
+                createdAt: new Date()
+            });
 
+            // clear form
+            event.target.text.value = "";
+
+            // prevent default form submit
+            return false;
+        }
+    });
 }
 
 if (Meteor.isServer) {
