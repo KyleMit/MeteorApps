71c7352122ab0960e4264ed4aa2d95124af7b189
Kyle <KyleMit@gmail.com>
2015-07-27 21:39:07 -0400

    Updated package version

--- packages/nametag/package.js
+++ packages/nametag/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kylemit:nametag',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Adds a hello nametag template',
   // URL to the Git repository containing the source code for this package.
