a53ab3fd783196aca484fa8cb92ef96ee79dbeef
Kyle <KyleMit@gmail.com>
2015-08-03 21:52:52 -0400

    Commit 7-8
    Sort by timestamp

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -1,5 +1,9 @@
 Template.postsList.helpers({
 	posts: function() {
-		return Posts.find();
+		var operator = {};
+		operator.sort = {
+			submitted: -1
+		};
+		return Posts.find({}, operator);
 	}
 });
